893bd77647cc47cd22500ff578cdc47782c282c3
Andre Ferreira <andrevcandido61@gmail.com>
2022-12-13 11:36:05 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "eeb88873ce624407000ed7d1ee160569"
+
+let language = "pt-BR"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
